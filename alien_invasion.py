618d5deca823328b2b874682a3d3fb71462d367f
stepheng87 <scooba75@gmail.com>
2025-06-24 23:14:49 -0500

    Make bullets hurt , a new fleet, and speed up bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -91,6 +91,16 @@ class AlienInvasion:
                     self.bullets.remove(bullet)
         print(len(self.bullets))
 
+        # Check for any bullets that have hit aliens.
+        # If so, get rid of the bullet and the alien.
+        collisions = pygame.sprite.groupcollide(
+                self.bullets, self.aliens, True, True)
+        
+        if not self.aliens:
+            # Destroy existing bullets and create new fleet.
+            self.bullets.empty()
+            self._create_fleet()
+
     def _update_aliens(self):
         """
         Check if the fleet is at an edge,
